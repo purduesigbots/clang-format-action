9e0e76b548f6d653b826e65ac9b33bce3fc6b34d
JayceeLock <jaycee.lock@gmail.com>
2022-08-03 08:59:29 +0100

    Modified dockerfile to adopt to user of the pulled-in files, rather that root. This solves the permission error more elegantly that setting safe dir or changing the ownership to root

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
 # Address fatal error with directory being owned by root
-git config --global --add safe.directory /github/workspace
+#git config --global --add safe.directory /github/workspace
 
 git-clang-format --style="$1" "$2"
 
