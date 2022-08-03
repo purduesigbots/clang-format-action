66bb824b38f8a89f02ea781b85d8bd22ce3c17ef
JayceeLock <jaycee.lock@gmail.com>
2022-08-03 09:11:35 +0100

    Cleanup

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,3 @@
 #!/bin/sh -l
 
-# Address fatal error with directory being owned by root
-#git config --global --add safe.directory /github/workspace
-
 git-clang-format --style="$1" "$2"
-
-ls -la .git/objects
